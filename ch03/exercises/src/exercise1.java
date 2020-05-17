7edad9f01e6558dfa0a7aacb1fb057bfafa9a32f
Julie Ann Dela Pena <delapena.julieann@hotmail.com>
2020-05-17 16:15:01 -0700

    exercise with object oriented programming

---
+++ ch03/exercises/src/exercise1.java
@@ -0,0 +1,24 @@
+class Computer {
+    // variables
+    double cpuSpeed; // in GHz
+    // constructor
+    Computer() {
+        cpuSpeed = 0;
+    }
+
+    // methods
+    void setCpuSpeed (double _cpuSpeed) {
+        cpuSpeed = _cpuSpeed;
+    }
+    double getCpuSpeed () {
+        return cpuSpeed;
+    }
+}
+
+public class exercise1 {
+    public static void main(String[] args) {
+        Computer myPC = new Computer();
+        myPC.setCpuSpeed(2.5);
+        System.out.println(myPC.getCpuSpeed());
+    }
+}
