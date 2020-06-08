5dfe47439e86012df62565e03ecb455b175988b0
Julie Ann Dela Pena <delapena.julieann@hotmail.com>
2020-06-07 19:24:49 -0700

    ch04 exercise

---
+++ ch04/exercises/src/exercise9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class exercise9 {
+    public static void main(String[] args) {
+        Integer[] numbers1 = new Integer[] {3, 25, 2, 79, 2};
+        Integer[] numbers2 = new Integer[] {7, 12, 14, 79};
+        Set set1 = new TreeSet();
+        Collections.addAll(set1, numbers1);
+        Set set2 = new TreeSet();
+        Collections.addAll(set2, numbers2);
+        set1.retainAll(set2);
+        System.out.println(set1);
+    }
+}
